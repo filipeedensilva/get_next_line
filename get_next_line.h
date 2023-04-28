78e07b968718b7a88215da5b58c70e9816b1ee40
filipeedensilva <filipeedensilva@gmail.com>
2023-04-28 17:04:15 +0100

    Adding files to repo

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: feden-pe <feden-pe@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 20:28:51 by feden-pe          #+#    #+#             */
+/*   Updated: 2023/04/27 20:40:11 by feden-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
